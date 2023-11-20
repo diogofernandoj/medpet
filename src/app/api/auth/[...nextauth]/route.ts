2346bbfc9bb852b0a550aca804af30ac04932ca5
Diogo Jorge <diogofernandoj@gmail.com>
2023-11-20 15:04:00 -0300

    feat: add authentication with next-auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,49 @@
+import { prismaClient } from "@/app/lib/prisma";
+import NextAuth, { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const authOptions: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+
+      async authorize(credentials, req) {
+        const user = await prismaClient.user.findFirst({
+          where: {
+            email: credentials?.email,
+            AND: {
+              password: credentials?.password,
+            },
+          },
+        });
+
+        if (user) {
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      user && (token.user = user);
+      return token;
+    },
+    async session({ session, token }) {
+      session = token.user as any;
+      return session;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
