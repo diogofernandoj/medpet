6d6c17f934ffae8701991d1462bc584d72981b39
Diogo Jorge <diogofernandoj@gmail.com>
2023-11-20 15:04:59 -0300

    feat: add LoginForm component and signIn function

--- src/app/(home)/components/login-form.tsx
+++ src/app/(home)/components/login-form.tsx
@@ -9,31 +9,64 @@ import {
   FormControl,
   FormMessage,
 } from "@/components/ui/form";
+import { Toaster } from "@/components/ui/toaster";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
+import { useToast } from "@/components/ui/use-toast";
+import { useState } from "react";
+import { Loader2Icon } from "lucide-react";
 
 const formSchema = z.object({
-  email: z.string().min(2, {
-    message: "Email must be at least 2 characters.",
+  email: z.string().email({ message: "Insira um e-mail válido" }),
+  password: z.string().min(6, {
+    message: "A senha deve conter ao menos 6 dígitos",
   }),
 });
 
 const LoginForm = () => {
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const [loading, setLoading] = useState<boolean>(false);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
+      password: "",
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    setLoading(true);
     console.log(values);
-  }
+
+    const res = await signIn("credentials", {
+      email: values.email,
+      password: values.password,
+      redirect: false,
+    });
+
+    if (res?.error) {
+      console.log(res);
+      toast({
+        description: "Os dados inseridos são inválidos!",
+        variant: "destructive",
+      });
+      setLoading(false);
+      return;
+    }
+
+    router.replace("/dashboard");
+  };
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <Toaster />
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
         <FormField
           control={form.control}
           name="email"
@@ -41,13 +74,44 @@ const LoginForm = () => {
             <FormItem>
               <FormLabel>Email</FormLabel>
               <FormControl>
-                <Input placeholder="Insira seu email" {...field} />
+                <Input
+                  placeholder="Insira seu email"
+                  {...field}
+                  className="rounded-3xl focus-visible:ring-1 focus-visible:ring-offset-0"
+                />
               </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Entrar</Button>
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Senha</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Insira sua senha"
+                  {...field}
+                  className="rounded-3xl focus-visible:ring-1 focus-visible:ring-offset-0"
+                  type="password"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          disabled={loading}
+          type="submit"
+          className={`w-full rounded-3xl font-bold ${
+            loading && "bg-opacity-75"
+          }`}
+        >
+          {loading ? <Loader2Icon className="animate-spin" /> : "Entrar"}
+        </Button>
       </form>
     </Form>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import LoginForm from "./(home)/components/login-form";
 
 const Home = () => {
   return (
-    <div className="flex flex-col gap-8 px-5 w-full max-w-[600px] mx-auto">
+    <div className="flex flex-col gap-8 px-5 w-full max-w-[600px] mx-auto mt-5">
       <div className="flex flex-col w-full">
         <Image
           src="/medpet.svg"
