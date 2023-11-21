cf33531d0f590d1016610a8f1ccc87ba58a878f3
Diogo Jorge <diogofernandoj@gmail.com>
2023-11-21 19:45:07 -0300

    feat: add CalendarDateRangePicker component

---
+++ src/app/(private-routes)/dashboard/components/date-range-picker.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import * as React from "react";
+import { addDays, format } from "date-fns";
+import { DateRange } from "react-day-picker";
+
+import { cn } from "@/lib/utils";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { CalendarIcon } from "lucide-react";
+import { Calendar } from "@/components/ui/calendar";
+
+const CalendarDateRangePicker = ({
+  className,
+}: React.HTMLAttributes<HTMLDivElement>) => {
+  const [date, setDate] = React.useState<DateRange | undefined>({
+    from: new Date(2023, 0, 20),
+    to: addDays(new Date(2023, 0, 20), 20),
+  });
+
+  return (
+    <div className={cn("grid gap-2", className)}>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            id="date"
+            variant={"outline"}
+            className={cn(
+              "w-max justify-start text-left font-medium text-xs",
+              !date && "text-muted-foreground"
+            )}
+          >
+            <CalendarIcon size={18} className="text-gray-800 lg:mr-2" />
+            <span className="hidden lg:block">
+              {date?.from ? (
+                date.to ? (
+                  <>
+                    {format(date.from, "LLL dd, y")} -{" "}
+                    {format(date.to, "LLL dd, y")}
+                  </>
+                ) : (
+                  format(date.from, "LLL dd, y")
+                )
+              ) : (
+                <span>Pick a date</span>
+              )}
+            </span>
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="end">
+          <Calendar
+            initialFocus
+            mode="range"
+            defaultMonth={date?.from}
+            selected={date}
+            onSelect={setDate}
+            numberOfMonths={2}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+};
+
+export default CalendarDateRangePicker;
--- src/app/(private-routes)/dashboard/page.tsx
+++ src/app/(private-routes)/dashboard/page.tsx
@@ -1,14 +1,20 @@
 import { authOptions } from "@/app/lib/auth";
 import { getServerSession } from "next-auth";
-import SignoutButton from "./components/signout-button";
+import CalendarDateRangePicker from "./components/date-range-picker";
 
 const Dashboard = async () => {
   const session = await getServerSession(authOptions);
 
   return (
-    <div>
-      <p>Olá {session?.user.name}, seja bem-vindo!</p>
-      <SignoutButton />
+    <div className="lg:pl-80">
+      <div className="flex flex-col">
+        <div className="flex items-center mt-4 px-5 gap-2 justify-between">
+          <h1 className="font-semibold text-xl lg:text-2xl text-gray-800">
+            Dashboard
+          </h1>
+          <CalendarDateRangePicker />
+        </div>
+      </div>
     </div>
   );
 };
