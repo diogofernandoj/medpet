0f800b58913aad251a498464bc2f81d1374bb81d
Diogo Jorge <diogofernandoj@gmail.com>
2023-12-18 18:18:00 -0300

    refactor: replace create transaction api for server actions

---
+++ src/app/(private-routes)/dashboard/_actions/create-transaction/index.ts
@@ -0,0 +1,75 @@
+"use server";
+
+import { authOptions } from "@/app/lib/auth";
+import { prismaClient } from "@/app/lib/prisma";
+import { transactionTypes } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { revalidatePath } from "next/cache";
+
+interface CreateTransactionProps {
+  title: string;
+  date: Date;
+  type: transactionTypes;
+  status: boolean;
+  amount: number;
+  installments: number;
+  notes: string;
+}
+
+export const createTransaction = async ({
+  title,
+  date,
+  type,
+  status,
+  installments,
+  amount,
+  notes,
+}: CreateTransactionProps) => {
+  const session = await getServerSession(authOptions);
+  const user_id = session?.user.id;
+
+  if (!user_id) {
+    return {
+      message: "User id is missing!",
+      statusCode: 400,
+    };
+  }
+
+  let data = [];
+
+  for (let i = 0; i < installments; i++) {
+    if (i === 0) {
+      data.push({
+        user_id,
+        title,
+        date: new Date(date),
+        type,
+        status,
+        amount,
+        notes,
+      });
+    } else {
+      data.push({
+        user_id,
+        title,
+        date: new Date(
+          new Date(date).getFullYear(),
+          new Date(date).getMonth() + i,
+          new Date(date).getDate()
+        ),
+        type,
+        status: false,
+        amount,
+        notes,
+      });
+    }
+  }
+
+  const transactions = await prismaClient.transaction.createMany({
+    data,
+  });
+
+  revalidatePath("/dashboard");
+
+  return { transactions, statusCode: 200 };
+};
--- src/app/(private-routes)/dashboard/page.tsx
+++ src/app/(private-routes)/dashboard/page.tsx
@@ -2,17 +2,23 @@ import { getServerSession } from "next-auth";
 import Cards from "./components/cards";
 import { authOptions } from "@/app/lib/auth";
 import DateRangeProvider from "@/app/providers/date-range";
-import DataTableWrapper from "./components/data-table-wrapper";
+import { DataTable } from "./transactions/data-table";
+import { columns } from "./transactions/columns";
+import { prismaClient } from "@/app/lib/prisma";
 
 const Dashboard = async () => {
   const session = await getServerSession(authOptions);
 
+  const transactions = await prismaClient.transaction.findMany({
+    where: { user_id: session?.user.id },
+  });
+
   return (
-    <div className="lg:px-80 pb-20 lg:pb-0">
+    <div className="lg:pl-80 pb-20 lg:pb-0">
       <DateRangeProvider userId={session?.user.id}>
         <Cards />
         <div className="mt-20 px-1 lg:px-5">
-          <DataTableWrapper />
+          <DataTable columns={columns} data={transactions} />;
         </div>
       </DateRangeProvider>
     </div>
