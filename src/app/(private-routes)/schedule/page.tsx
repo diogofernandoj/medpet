c611f87a54cb59076a861a7824e978808dc5ddcb
Diogo Jorge <diogofernandoj@gmail.com>
2024-01-20 20:05:21 -0300

    feat: add SchedulePage

---
+++ src/app/(private-routes)/schedule/page.tsx
@@ -0,0 +1,9 @@
+const SchedulePage = () => {
+  return (
+    <div className="lg:pl-80 pb-20 lg:pb-0 flex justify-center items-center h-[100vh] font-bold text-lg text-primary">
+      Em breve.
+    </div>
+  );
+};
+
+export default SchedulePage;
