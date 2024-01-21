58288f6a9b34c370e07f08b897d7724ed8e72c57
Diogo Jorge <diogofernandoj@gmail.com>
2024-01-20 21:06:31 -0300

    feat: add newClient server action

---
+++ src/app/(private-routes)/clients/_actions/add-client/index.ts
@@ -0,0 +1,47 @@
+"use server";
+
+import { authOptions } from "@/app/lib/auth";
+import { prismaClient } from "@/app/lib/prisma";
+import { getServerSession } from "next-auth";
+import { revalidatePath } from "next/cache";
+
+interface addClientProps {
+  name: string;
+  document: string;
+  phone: string;
+  address: string;
+  notes: string;
+}
+
+export const addClient = async ({
+  name,
+  document,
+  phone,
+  address,
+  notes,
+}: addClientProps) => {
+  const session = await getServerSession(authOptions);
+  const user_id = session?.user.id;
+
+  if (!user_id) {
+    return {
+      message: "User id is missing!",
+      statusCode: 400,
+    };
+  }
+
+  const client = await prismaClient.client.create({
+    data: {
+      user_id,
+      name,
+      document,
+      phone,
+      address,
+      notes,
+    },
+  });
+
+  revalidatePath("/clients");
+
+  return { client, statusCode: 200 };
+};
