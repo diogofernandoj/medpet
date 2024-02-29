f689fa84d86b1a55bd6b87262474c80c888c0068
Diogo Jorge <diogofernandoj@gmail.com>
2024-02-28 21:18:26 -0300

    feat: add ToggleStatusButton

---
+++ src/app/(private-routes)/dashboard/components/toggle-status-button.tsx
@@ -0,0 +1,26 @@
+import { ReactNode, useContext } from "react";
+import { toggleTransactionStatus } from "../_actions/toggle-transaction-status";
+import { DateRangeContext } from "@/app/providers/date-range";
+
+interface ToggleStatusButtonProps {
+  children: ReactNode;
+  transactionId: string;
+  status: boolean;
+}
+
+const ToggleStatusButton = ({
+  children,
+  transactionId,
+  status,
+}: ToggleStatusButtonProps) => {
+  const { setRevalidateTransactions } = useContext(DateRangeContext);
+
+  const handleStatusClick = () => {
+    toggleTransactionStatus({ transactionId, status });
+    setRevalidateTransactions((prev) => prev + 1);
+  };
+
+  return <button onClick={handleStatusClick}>{children}</button>;
+};
+
+export default ToggleStatusButton;
