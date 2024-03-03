0ce5f0345caffed4b77cd720e2fa3c04e55ea60a
Diogo Jorge <diogofernandoj@gmail.com>
2024-03-03 08:46:41 -0300

    feat: add new transaction button to client history

--- src/app/(private-routes)/clients/[clientId]/components/client-history.tsx
+++ src/app/(private-routes)/clients/[clientId]/components/client-history.tsx
@@ -1,3 +1,4 @@
+import AddTransactionButton from "@/app/(private-routes)/dashboard/components/add-transaction-button";
 import { getClientTransactions } from "../../_actions/get-client-transactions";
 import { columns } from "../../history/columns";
 import { DataTable } from "../../history/data-table";
@@ -7,8 +8,11 @@ const ClientHistory = async ({ clientId }: { clientId: string }) => {
   const data = res?.transactions || [];
 
   return (
-    <div className="flex flex-col gap-8">
-      <DataTable columns={columns} data={data} />
+    <div className="flex flex-col w-full mx-auto px-12 my-6 items-center">
+      <AddTransactionButton client_id={clientId} />
+      <div className="w-full">
+        <DataTable columns={columns} data={data} />
+      </div>
     </div>
   );
 };
